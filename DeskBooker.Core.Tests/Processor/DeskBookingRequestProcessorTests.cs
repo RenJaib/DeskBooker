acadd421f5b560d18f8e95fb8b7f58cd66ebe57b
Renju Devassy <renjudevasy@gmail.com>
2023-09-21 23:55:31 +1200

    Add project files.

---
+++ DeskBooker.Core.Tests/Processor/DeskBookingRequestProcessorTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DeskBooker.Core.Tests.Domain;
+
+namespace DeskBooker.Core.Tests.Processor
+{
+    public class DeskBookingRequestProcessorTests
+    {
+        [Test]
+        public void ShouldReturnDeskBookingResultWithRequestValues()
+        {
+            //arrange
+            var request = new DeskBookingRequest
+            {
+                FirstName = "Renju",
+                LastName = "Devassy",
+                Email = "test@testmail.com",
+                Date = new DateTime(2023, 09, 21)
+            };
+            var processor = new DeskBookingRequestProcessor();
+
+            //act
+            DeskBookingResult result = processor.BookDesk(request);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(request.FirstName, result.FirstName);
+            Assert.AreEqual(request.LastName, result.LastName);
+            Assert.AreEqual(request.Email, result.Email);
+            Assert.AreEqual(request.Date, result.Date);
+
+        }
+
+        [Test]
+        public void ShouldThrowExceptionWhenDeskBookingRequestValueIsNull()
+        {
+            var processor = new DeskBookingRequestProcessor();
+
+            //act
+            var result = Assert.Throws<ArgumentNullException>(()=>processor.BookDesk(null));
+
+            //assert
+            Assert.AreEqual("request", result.ParamName);
+
+        }
+
+    }
+}
